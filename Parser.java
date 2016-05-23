4fa803e64ebfbca9f696f49d0735b5c4a3f34daa
danielat998 <danielt998@gmail.com>
2016-05-24 00:59:32 +0100

    did more stuff

--- Parser.java
+++ Parser.java
@@ -1,27 +1,29 @@
 import java.util.HashMap;
+/* As a guideline, see:https://en.wikipedia.org/wiki/Tiny_BASIC but note we are not necessarily
+ aiming to implement this perfectly*/
 public class Parser{
 	//only use this for testing
 	public static void main(String[] args){
-		String[] lines = {"0 PRINT 10", "10 print hello"};
+		String[] lines = {"0 PRINT 10 20 hello goodbye", "10 goto 0"};
 		Prog prog = parse(lines);
 		System.out.println(prog.instructions[0] + "\n" + prog.instructions[10]);
 	}
 
 	public static enum Command {
-		PRINT,INPUT,GOTO,IF,THEN//...
+		PRINT,INPUT,GOTO,IF,THEN,LET//...
 	}
 
 	public static class Inst{
 		public int lineNo;//same as array index
 		public Command command;
-		public String param;
+		public String[] params;
 		@Override
 		public String toString(){
-			return "lineNo:"+lineNo + ", command:" + command +", param:" + param;
+			String prms = "";
+			for (String p:params)
+				prms += " " + p;
+			return "lineNo:"+lineNo + ", command:" + command +", params:" + prms;
 		}
-		/*public Inst(int line, String command, ...){
-			lineNo = line;
-		}*/
 	}
 
 	public static class Prog{//keep things public and simple
@@ -39,17 +41,45 @@ public class Parser{
   public static Prog parse(String[] lines){
 		Prog prog = new Prog();
 		prog.instructions = new Inst[getLastLineNo(lines) + 1];
+		boolean lastLineWasIf = false;
 
 		for (String line:lines){
 			String[] tokens = line.split(" ");
 			Inst inst = new Inst();
 			inst.lineNo = Integer.parseInt(tokens[0]);
 			prog.instructions[inst.lineNo] = inst;
+			if (lastLineWasIf){//must wait for endif...
+				//inst.command 
+				continue;
+			}
 			switch(tokens[1]){//actual command
 				case "PRINT":case "print":
 						inst.command = Command.PRINT;
-						inst.param = tokens[2];
+						inst.params = new String[tokens.length-2];
+						for (int i = 0; i < tokens.length -2;i++)
+							inst.params[i] = tokens[i+2];
 						break;
+				case "GOTO":case"goto":
+						inst.command = Command.GOTO;
+						inst.params = new String[]{tokens[2]};
+						break;
+				case "IF":case"if":
+						inst.command = Command.IF;
+						inst.params = new String[]{tokens[2],tokens[3],tokens[4]};
+						//gonna be complicated, not sure whether this belongs in the parse or interpreter
+						break;
+				case "INPUT":case "input":
+						inst.command = Command.INPUT;
+						inst.params = new String[tokens.length-2];
+						for (int i = 0; i < tokens.length -2;i++)
+							inst.params[i] = tokens[i+2];
+						break;
+				case "LET":case "let":
+						inst.command = Command.LET;
+						//finish this, again think at least part of this belongs in interpreter...
+						//TODO:GOSUB,RETURN,CLEAR,LIST,RUN,END
+				default:
+						System.out.println("Unrecognised command, please try again");			
 			}
 		}
 		return prog;
