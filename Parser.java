8acd053f0b38cfd33c0851139193c726c98fe3ed
danielat998 <danielt998@gmail.com>
2016-05-24 23:46:52 +0100

    added if statement support, hopefully it wil work for nested IFs too :D

--- Parser.java
+++ Parser.java
@@ -1,22 +1,32 @@
 import java.util.HashMap;
+import java.util.Stack;
 /* As a guideline, see:https://en.wikipedia.org/wiki/Tiny_BASIC but note we are not necessarily
  aiming to implement this perfectly*/
+/*Language simplifications:
+for now, there must be a space between LET X = 5 etc
+no ELSEs
+*/
 public class Parser{
 	//only use this for testing
 	public static void main(String[] args){
-		String[] lines = {"0 PRINT 10 20 hello goodbye", "10 goto 0"};
+		String[] lines = {"0 PRINT 10 20 hello goodbye", "10 goto 0", "11 IF a = 4 THEN", "12 GOTO 10",
+					 																																						"13 END"};
 		Prog prog = parse(lines);
-		System.out.println(prog.instructions[0] + "\n" + prog.instructions[10]);
+		System.out.println(prog.instructions[0] + "\n" + prog.instructions[10] + "\n" + 
+							         prog.instructions[11] +"\n\t" + prog.instructions[11].matchingIfOrEnd + "\n"
+											 + prog.instructions[12] + "\n"+ prog.instructions[13] +
+											 "\n\t"+ prog.instructions[13].matchingIfOrEnd);
 	}
 
 	public static enum Command {
-		PRINT,INPUT,GOTO,IF,THEN,LET//...
+		PRINT,INPUT,GOTO,IF,THEN,LET,END//...
 	}
 
 	public static class Inst{
 		public int lineNo;//same as array index
 		public Command command;
-		public String[] params;
+		public String[] params = new String[]{""};
+		public Inst matchingIfOrEnd;
 		@Override
 		public String toString(){
 			String prms = "";
@@ -41,17 +51,17 @@ public class Parser{
   public static Prog parse(String[] lines){
 		Prog prog = new Prog();
 		prog.instructions = new Inst[getLastLineNo(lines) + 1];
-		boolean lastLineWasIf = false;
+		Stack<Inst> ifs = new Stack<Inst>();
 
 		for (String line:lines){
 			String[] tokens = line.split(" ");
 			Inst inst = new Inst();
 			inst.lineNo = Integer.parseInt(tokens[0]);
 			prog.instructions[inst.lineNo] = inst;
-			if (lastLineWasIf){//must wait for endif...
+			/*if (ifLevel != 0){//must wait for endif...
 				//inst.command 
 				continue;
-			}
+			}*/
 			switch(tokens[1]){//actual command
 				case "PRINT":case "print":
 						inst.command = Command.PRINT;
@@ -66,7 +76,15 @@ public class Parser{
 				case "IF":case"if":
 						inst.command = Command.IF;
 						inst.params = new String[]{tokens[2],tokens[3],tokens[4]};
+						ifs.push(inst);
 						//gonna be complicated, not sure whether this belongs in the parse or interpreter
+						break;
+				case "END":case "end":case "ENDIF":case "endif":
+						//pop from if stack to get most recent if to match up
+						inst.command = Command.END;
+						Inst matchingIf = ifs.pop();
+						matchingIf.matchingIfOrEnd = inst;
+						inst.matchingIfOrEnd = matchingIf;
 						break;
 				case "INPUT":case "input":
 						inst.command = Command.INPUT;
@@ -76,8 +94,11 @@ public class Parser{
 						break;
 				case "LET":case "let":
 						inst.command = Command.LET;
+						inst.params[0] = tokens[2];
+						inst.params[1] = tokens[4];//ignore '='
 						//finish this, again think at least part of this belongs in interpreter...
 						//TODO:GOSUB,RETURN,CLEAR,LIST,RUN,END
+						break;
 				default:
 						System.out.println("Unrecognised command, please try again");			
 			}
