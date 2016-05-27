b4ae86eaa7032579841b0aafdc44a6a0041d36d8
danielat998 <danielt998@gmail.com>
2016-05-27 23:48:20 +0100

    print now has variable support though no parsing

--- Run.java
+++ Run.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.nio.file.*;
+import java.util.*;
 public class Run{
 	public static void main(String[] args){
 		try{
@@ -17,21 +18,31 @@ public class Run{
 			}
 	}
 	public static void run(Parser.Prog prog){
-		for (Parser.Inst i : prog.instructions){
+//		for (Parser.Inst i : prog.instructions){
+		Scanner input = new Scanner(System.in);
+		for (int pc = 0; pc < prog.instructions.length;pc++){
+			Parser.Inst i = prog.instructions[pc];
 			if (i == null)//we want to skip non-existant line numbers
 				continue;
-				if (i.command == Parser.Command.PRINT){
-					print(i);
-					continue;
-				} else {
-					System.out.println("  command "+i.command +" not recognised");
+			if (i.command == Parser.Command.PRINT){
+				for (String s : i.params){
+					if (s.substring(0,1).equals("$")){
+						System.out.print(prog.vars.get(s.substring(1,s.length())) + " ");
+					} else {
+						System.out.print(s + " ");
+					}
 				}
-		}
-	}
-
-	public static void print(Parser.Inst inst){
-		for (int i= 0; i < inst.params.length; i++)
-			System.out.print(inst.params[i] + " ");
-		System.out.println();
-	}
-}
+				System.out.println();
+				continue;
+			} else if (i.command == Parser.Command.GOTO){
+				pc = Integer.parseInt(i.params[0]) -1;//-1 because it wil get incremented at end of loop
+				continue;
+			} else if (i.command == Parser.Command.INPUT){
+				prog.vars.put(i.params[0], input.nextLine());
+				continue;
+			} else {
+				System.out.println("  command "+i.command +" not recognised");
+			}
+		}//pc loop
+	}//run
+}//class
