707859a61ed6aaa8a4e4c26eeabf990d4d8ce345
danielat998 <danielt998@gmail.com>
2016-05-28 09:03:44 +0100

    got IFs working!! :D

--- Run.java
+++ Run.java
@@ -40,9 +40,61 @@ public class Run{
 			} else if (i.command == Parser.Command.INPUT){
 				prog.vars.put(i.params[0], input.nextLine());
 				continue;
+			} else if (i.command == Parser.Command.IF){
+				if (statementTrue(prog, i.params[0], i.params[1], i.params[2]))
+					continue;
+				else{
+					pc = i.matchingIfOrEnd.lineNo - 1;//skip to ENDIF
+					continue;
+				}
+			}	else if (i.command == Parser.Command.ENDIF){
+					continue;
 			} else {
 				System.out.println("  command "+i.command +" not recognised");
 			}
 		}//pc loop
 	}//run
+
+	public static boolean statementTrue(Parser.Prog prog, String x, String comp, String y){
+		//first of all, check if x and/or y are variables, if so, retrieve their values and replace
+		//x or y with that value
+		if (x.substring(0,1).equals("$")){
+			x = prog.vars.get(x.substring(1,x.length()));
+			if (x == null){
+				System.out.println("$"+ x +
+							"does not exist or is null, treating statment as false and jumping to next ENDIF");
+				return false;
+			}
+		}
+		if (y.substring(0,1).equals("$")){
+			y = prog.vars.get(y.substring(1,y.length()));
+			if (y == null){
+				System.out.println("$"+ y +
+							"does not exist or is null, treating statment as false and jumping to next ENDIF");
+				return false;
+			}
+		}
+		//for now we will assume int and String are the only supported types (may be true, check spec)
+		//try for integers, failing that, fall back to Strings, note both should be the same, but if we 
+		//encounter a type mismatch, the expression should evaluate as false anyway
+		try{
+			int xInt = Integer.parseInt(x);
+			int yInt = Integer.parseInt(y);
+			if (comp.equals("<"))
+				return xInt < yInt;
+			if (comp.equals("="))
+					return xInt == yInt;
+			if (comp.equals(">"))
+					return xInt > yInt;
+		} catch (Exception e){
+			//use default java string comparison for now
+			if (comp.equals("<"))
+				return x.compareTo(y) < 0;
+			if (comp.equals("="))
+				return x.equals(y);
+			if (comp.equals(">"))
+				return x.compareTo(y) > 0;
+		}
+		return false;
+	}
 }//class
